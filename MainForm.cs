758fc4e3602ee86cbd78cb2b07cec2b41b609d87
Roman Khadraliev <Romch33tos@yandex.ru>
2025-09-08 15:11:02 +0700

    Реализована возможность выбора файла

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
 
@@ -10,6 +10,7 @@ namespace SportsCompetitionApp
     event EventHandler LoadDataClicked;
     event EventHandler FilterByYearClicked;
     event EventHandler FindBestAthleteClicked;
+    event EventHandler BrowseFileClicked;
 
     string FilePath { get; set; }
     int FilterYear { get; set; }
@@ -18,6 +19,10 @@ namespace SportsCompetitionApp
     void DisplayFilteredAthletes(List<Athlete> athletes);
     void DisplayBestAthlete(Athlete athlete);
     void ShowMessage(string message);
+    void DisableGenerateButton();
+    void EnableFilterButtons(bool enable);
+    string ShowSaveFileDialog();
+    string ShowOpenFileDialog();
   }
 
   public partial class MainForm : Form, IMainView
@@ -26,12 +31,18 @@ namespace SportsCompetitionApp
     public event EventHandler LoadDataClicked;
     public event EventHandler FilterByYearClicked;
     public event EventHandler FindBestAthleteClicked;
+    public event EventHandler BrowseFileClicked;
+
+    private const int MIN_YEAR = 1970;
+    private const int MAX_YEAR = 2006;
 
     public MainForm()
     {
       InitializeComponent();
       SetupDataGridView();
       SubscribeEvents();
+      SetupYearNumericUpDown();
+      EnableFilterButtons(false);
     }
 
     private void SetupDataGridView()
@@ -42,12 +53,22 @@ namespace SportsCompetitionApp
       dataGridViewAthletes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
     }
 
+    private void SetupYearNumericUpDown()
+    {
+      numericUpDownYear.Minimum = MIN_YEAR;
+      numericUpDownYear.Maximum = MAX_YEAR;
+      numericUpDownYear.Value = MIN_YEAR;
+
+      numericUpDownYear.ReadOnly = true; 
+    }
+
     private void SubscribeEvents()
     {
       buttonGenerate.Click += (s, e) => GenerateDataClicked?.Invoke(this, EventArgs.Empty);
       buttonLoad.Click += (s, e) => LoadDataClicked?.Invoke(this, EventArgs.Empty);
       buttonFilter.Click += (s, e) => FilterByYearClicked?.Invoke(this, EventArgs.Empty);
       buttonFindBest.Click += (s, e) => FindBestAthleteClicked?.Invoke(this, EventArgs.Empty);
+      buttonBrowse.Click += (s, e) => BrowseFileClicked?.Invoke(this, EventArgs.Empty);
     }
 
     public string FilePath
@@ -58,8 +79,17 @@ namespace SportsCompetitionApp
 
     public int FilterYear
     {
-      get => int.TryParse(textBoxYear.Text, out int year) ? year : 0;
-      set => textBoxYear.Text = value.ToString();
+      get
+      {
+        return (int)numericUpDownYear.Value;
+      }
+      set
+      {
+        if (value >= MIN_YEAR && value <= MAX_YEAR)
+        {
+          numericUpDownYear.Value = value;
+        }
+      }
     }
 
     public void DisplayAllAthletes(List<Athlete> athletes)
@@ -86,6 +116,46 @@ namespace SportsCompetitionApp
     public void ShowMessage(string message)
     {
       MessageBox.Show(message, "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    public void DisableGenerateButton()
+    {
+      buttonGenerate.Enabled = false;
+    }
+
+    public void EnableFilterButtons(bool enable)
+    {
+      buttonFilter.Enabled = enable;
+      buttonFindBest.Enabled = enable;
+      numericUpDownYear.Enabled = enable;
+
+      if (!enable)
+      {
+        numericUpDownYear.Value = MIN_YEAR;
+      }
+    }
+
+    public string ShowSaveFileDialog()
+    {
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        return saveFileDialog.FileName;
+      }
+      return null;
+    }
+
+    public string ShowOpenFileDialog()
+    {
+      if (openFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        return openFileDialog.FileName;
+      }
+      return null;
+    }
+
+    private void buttonFilter_Click(object sender, EventArgs e)
+    {
+
     }
   }
 }
